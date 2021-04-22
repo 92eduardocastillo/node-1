b94f65e0b448cf7c24bcbe241476b1663ca206b8
Castle <92eduardo castillo@gmail.com>
2021-04-22 14:53:13 -0400

    Agragando puerto

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended : false }))
 
 app.use(bodyParser.json())
 
-const port = 3005;
+const port = process.env.PORT || 3005;
 
 const mongoose = require('mongoose');
 
